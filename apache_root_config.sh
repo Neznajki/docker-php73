34e51d2f878f9fcfffc34c3f95221e335fc32141
Maris Locmelis <m.locmelis@dynatech.lv>
2019-10-02 21:33:18 +0300

    changed launching user to developer (to avoid root) optimized script build added nginx configuration guide

--- apache_root_config.sh
+++ apache_root_config.sh
@@ -3,4 +3,4 @@ set -e
 
 sed -i "s|{{ APACHE_DOCUMENT_ROOT }}|${APACHE_DOCUMENT_ROOT}|" /etc/apache2/sites-enabled/*.conf
 
-source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND
+sudo bash -c "source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND"
